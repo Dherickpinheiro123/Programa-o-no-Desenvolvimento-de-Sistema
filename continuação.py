9d8a5bbb99c34ef3fb3b21618249af5536ec01fa
Dherickpinheiro123 <pinheiro.dherick@escola.pr.gov.br>
2026-03-09 11:17:50 -0300

    Add files via upload

---
+++ continuação.py
@@ -0,0 +1,11 @@
+#variavel é um espaço de memória que tem um nome e guarda algum dado de programa
+numero = 1
+resultado = numero + numero + 5
+
+#Expressoes sao quaisquer combinacoes de
+# valor, variável e operadores
+resultado = numero + numero + 5
+resultado = (numero * (numero * numero) + 33)
+
+# A instrução print mostra o programa no terminal
+print(numero)
